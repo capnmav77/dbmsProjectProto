7b9b27aef5ab174b1348fb245a1545fc45d40428
capnmav77 <its.mastermind77@gmail.com>
2023-11-03 12:02:25 +0530

    profile page

--- Pages/5_Profle.py
+++ Pages/5_Profle.py
@@ -18,6 +18,41 @@ def connect_to_database():
         st.error(f"Error: {e}")
         return None
     
+def update_user_info(email,street,city,state,gender,password,about,new_password_updation = False):
+    if(new_password_updation == True):
+        st.text("updated password with user info")
+        #code for changing user information
+    else:
+        st.text("Updated user_info")
+        #code for changing user information with password
+
+
+def display_user_prof(user_info):
+    email =  st.text_input('Email',user_info[1])
+    Street = st.text_input('Street',user_info[2])
+    City = st.text_input('City',user_info[3])
+    State = st.text_input('State',user_info[4])
+    st.text(f"Gender: {user_info[5]}")
+    Gender = st.radio(f'Gender:{user_info[5]}',options=["Male","Female","Prefer not to say"],horizontal=True,)
+    st.text(f"selected:{Gender}")
+    Password = st.text_input('Password',user_info[6])
+    Password2 = st.text_input('Password Confirmation',user_info[6])
+    About = st.text_input('About',user_info[7])
+    if(user_info[9]==1):
+        st.text("✅ Pro membership activated.")
+    else: 
+        st.text("try out the Pro membership")
+    if(st.button('make changes')):
+        if(Password != user_info[6]):
+            if(Password != Password2):
+                st.warning("New Passwords don't match try again")
+            else:
+                update_user_info(email,Street,City,State,Gender,Password,About,new_password_updation=True)
+        else:
+            update_user_info(email,Street,City,State,Gender,Password,About,new_password_updation=False)
+                        
+            
+
 
 # Check if the user is logged in
 if 'Username' not in st.session_state:
@@ -27,30 +62,15 @@ User1 = st.session_state['Username']
 
 if User1 != "":
     User1 = st.session_state['Username']
-    st.text(f"Hey, {User1}, feel like changing your profile")
-    
-    # Display the user's groups here
+    st.subheader(f"Hey, {User1}, feel like changing your profile?")
     connection = connect_to_database()
     cursor = connection.cursor()
-    cursor.execute(f"call get_user_groups('{User1}')")
+    cursor.execute(f"SELECT * FROM users WHERE username = '{User1}'")
     result = cursor.fetchall()
-    print(result)
-    #creating a dropdown menu for the user to select a group
-    # Extract group names from the results
-    group_names = [group[1] for group in result]
+    connection.close()
+    display_user_prof(result[0])
     
-    if group_names:
-        # Create a selectbox with group names
-        selected_group_name = st.selectbox("Your Groups :", group_names)
-        #st.text(f"You selected {selected_group_name}")
-        
-        # Find the corresponding group_id for the selected group name
-        selected_group_id = [group[0] for group in result if group[1] == selected_group_name][0]
-        
-        # Call the get_group_events function with the selected group_id
-        get_group_details(selected_group_id)
-        st.write("---")
-        get_group_events(selected_group_id)
+
 
 else:
     st.warning("You need to log in to access this page.")
